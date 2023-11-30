eede21c9b09fe118655dea48d39ad3958be00310
Fan Wang <nimadorostkar97@gmail.com>
2023-11-30 10:09:31 +0330

    migrations

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from .models import User
 
-
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('phone_number', 'user_type', 'created_at')
-    #search_fields = ['name', 'province']
+    list_display = ('phone_number', 'created_at')
 admin.site.register(User, UserAdmin)
 
