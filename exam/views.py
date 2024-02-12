837136404200739cc278d0f8986d6e981edc9497
Fan Wang <nimadorostkar97@gmail.com>
2024-02-12 10:54:08 +0330

    CustomPagination pagesize

--- exam/views.py
+++ exam/views.py
@@ -18,9 +18,9 @@ from teacher.models import Teacher, ReserveTimes
 
 
 class CustomPagination(PageNumberPagination):
-    page_size = 10
+    page_size = 1000
     page_size_query_param = 'page_size'
-    max_page_size = 100
+    max_page_size = 1000
 
 
 
