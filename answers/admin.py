a8b58ccb4c97f4dc91b6daf342dd416701c08f88
Fan Wang <nimadorostkar97@gmail.com>
2024-02-21 09:26:11 +0330

    Test Full Correct Answer

--- answers/admin.py
+++ answers/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from answers.models import TestCorrectAnswer
+from answers.models import TestCorrectAnswer, TestFullCorrectAnswer, Answer
 from django.db import models
 
 
@@ -8,3 +8,19 @@ class TestCorrectAnswerAdmin(admin.ModelAdmin):
     list_filter = ('skill', 'book', 'skill')
     search_fields = ['name',]
 admin.site.register(TestCorrectAnswer, TestCorrectAnswerAdmin)
+
+
+
+
+
+
+
+class AnswerInline(admin.TabularInline):
+    model = Answer
+    extra = 1
+
+class TestFullCorrectAnswerAdmin(admin.ModelAdmin):
+    list_display = ('name','skill', 'type', 'book')
+    inlines = [AnswerInline, ]
+admin.site.register(TestFullCorrectAnswer, TestFullCorrectAnswerAdmin)
+
--- answers/models.py
+++ answers/models.py
@@ -16,3 +16,33 @@ class TestCorrectAnswer(models.Model):
 
     def __str__(self):
         return str(self.name) +'|'+ str(self.book)
+
+
+
+
+
+
+class TestFullCorrectAnswer(models.Model):
+    name = models.CharField(max_length=128, null=True, blank=True)
+    skill_choices = (("listening", "listening"), ("reading", "reading"), ("writing", "writing"))
+    skill = models.CharField(choices=skill_choices, max_length=128)
+    type_choices = (("academic", "academic"), ("general", "general"))
+    type = models.CharField(choices=type_choices, max_length=128)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.name) +'|'+ str(self.book)
+
+
+
+
+class Answer(models.Model):
+    test_answer = models.ForeignKey(TestFullCorrectAnswer,on_delete=models.CASCADE)
+    question_number = models.IntegerField()
+    answer = models.CharField(max_length=256, null=True, blank=True)
+    question = models.CharField(max_length=256, null=True, blank=True)
+    keywords = models.CharField(max_length=256, null=True, blank=True)
+    full_answer = models.TextField(max_length=2500, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.answer) + ' | ' + str(self.test_answer)
